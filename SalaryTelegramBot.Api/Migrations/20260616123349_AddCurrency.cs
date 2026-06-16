fb0c0aea00f0e01447cb7c1e19ea16f7bda52548
AFURSIKOV\alex <alex.furiskov@gmail.com>
2026-06-16 15:45:34 +0300

    fix: AddCurrency migration was empty - add column SQL

--- SalaryTelegramBot.Api/Migrations/20260616123349_AddCurrency.cs
+++ SalaryTelegramBot.Api/Migrations/20260616123349_AddCurrency.cs
@@ -10,13 +10,21 @@ namespace SalaryTelegramBot.Api.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "BotSettings",
+                type: "text",
+                nullable: false,
+                defaultValue: "RUB");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "BotSettings");
         }
     }
 }
+
