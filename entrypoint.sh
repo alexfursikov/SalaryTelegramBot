449e136242d9251b2c7d01e94136bb4f8cd4c580
AFURSIKOV\alex <alex.furiskov@gmail.com>
2026-07-24 21:48:05 +0300

    fix: set TELEGRAM__Token env var for DI registration

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 export ConnectionStrings__DefaultConnection="Host=postgres;Port=5432;Database=salary_bot;Username=admin;Password=${DB_PASSWORD}"
+export TELEGRAM__Token="${BOT_TOKEN}"
 export TELEGRAM_BOT_TOKEN="${BOT_TOKEN}"
 
 exec dotnet SalaryTelegramBot.Api.dll
